488dfa48933fe49450120bcf1efc18434666700d
emilyphalperin@gmail.com <emilyhalperin@Emilys-MacBook-Pro-2.local>
2023-01-13 09:46:37 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  let text = document.querySelector('#text')
+  text.textContent = "This is really cool!"
+})
+//first set up a dom content loader event listener 
+//use the callback function to target the paragraph with element id ='text'
+//then change the text to .... using text content 
